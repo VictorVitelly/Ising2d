492d897fda6cdadee0d3c5f5ad24da88363f5fe9
VictorVitelly_ICN <victor-vitelly@ciencias.unam.mx>
2025-02-28 14:19:56 -0600

    Created branch to measure susceptibility and heat capacity.

--- src/measurements.f90
+++ src/measurements.f90
@@ -6,11 +6,16 @@ module measurements
 
 contains
 
-  subroutine measure(spin,E,M)
+  subroutine measure(spin,E,M,susc1,susc2,heat1,heat2)
     integer(i4), dimension(:,:), intent(in) :: spin
     real(dp), intent(out) :: E, M
+    real(dp), intent(inout) :: susc1,susc2,heat1,heat2
+    real(dp) :: MM
+      MM=Magnet(spin)
       E=Hamilt(spin)
-      M=abs(Magnet(spin))
+      M=abs(MM)
+      susc1=susc1+(MM**2)
+      susc2=susc2+M
   end subroutine measure
 
 end module measurements
